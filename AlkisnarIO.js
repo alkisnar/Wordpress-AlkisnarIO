fba84d9ab2927930a78e2cfd2c82a13efe31979f
alkisnar <45603926+alkisnar@users.noreply.github.com>
2020-11-26 01:57:35 -0500

    Update AlkisnarIO.js

--- AlkisnarIO.js
+++ AlkisnarIO.js
@@ -1,21 +1,4 @@
-//DnyamoDB Counter for the GatewayAPI for the landing page
-
-URL myurl = new URL("https://b7kwlfbchk.execute-api.us-east-1.amazonaws.com/Version1/WebsiteHitCounterDynamoDBupdate")
-URLConnection myURLConnection = myURL.openConnection();
-    myURLConnection.connect();
-
-<script>
-
-    URL url = new URL("https://b7kwlfbchk.execute-api.us-east-1.amazonaws.com/Version1/WebsiteHitCounterDynamoDBupdate");
-   HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-   try {
-     InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-     readStream(in);
-   } finally {
-     urlConnection.disconnect();
-   }
-   
-</script>
+//Javascript page for future use when needed
 
 
 
