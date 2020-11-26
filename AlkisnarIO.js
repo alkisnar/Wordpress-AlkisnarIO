15404d221f56623d6f93eed404dfd6b5aa1f139c
alkisnar <45603926+alkisnar@users.noreply.github.com>
2020-11-26 00:14:29 -0500

    update
    update of JS and adding picture

--- AlkisnarIO.js
+++ AlkisnarIO.js
@@ -1,10 +1,21 @@
 //DnyamoDB Counter for the GatewayAPI for the landing page
 
-fetch('https://pxby95zsx5.execute-api.us-east-1.amazonaws.com/prod/')
+URL myurl = new URL("https://b7kwlfbchk.execute-api.us-east-1.amazonaws.com/Version1/WebsiteHitCounterDynamoDBupdate")
+URLConnection myURLConnection = myURL.openConnection();
+    myURLConnection.connect();
 
+<script>
 
-
-
+    URL url = new URL("https://b7kwlfbchk.execute-api.us-east-1.amazonaws.com/Version1/WebsiteHitCounterDynamoDBupdate");
+   HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+   try {
+     InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+     readStream(in);
+   } finally {
+     urlConnection.disconnect();
+   }
+   
+</script>
 
 
 
